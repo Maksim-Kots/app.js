55865040fd2dcf2d76c4d6de450e9f34b811e80e
Maksim-Kots <kots-maks@mail.ru>
2023-02-18 08:00:08 +0300

    3 commit

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const app = express();
+const contacts = require('./view/pages/aboutMe')
+const contacts = require('./view/pages/contacts')
+const contacts = require('./view/pages/mainPage')
+const contacts = require('./view/pages/shop')
 
-const pageContacts = require("./view/contacts")
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static('./public'))
@@ -15,6 +18,7 @@ app.get('/contacts', (req, res) => {
     }
     res.send(pageContacts)
 })
+
 
 app.listen(8000, () => {
     console.log("Сервер запущен на порту 8000.")
